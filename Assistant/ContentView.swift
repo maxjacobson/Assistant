be45a1411c46677da5debe71e778df0b4fa19d50
Max Jacobson <max@hardscrabble.net>
2023-10-14 22:47:49 -0400

    Add MVP code

--- Assistant/ContentView.swift
+++ Assistant/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Hire an assistant") {
+                let url = URL(string: "https://www.google.com/search?q=hire+personal+assistant+in+my+area")!
+                UIApplication.shared.open(url)
+            }.padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+
+
+            Text("(This will fix all of your problems)")
         }
         .padding()
     }
