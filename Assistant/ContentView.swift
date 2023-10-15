14cdc360ba9b058c3f9dcb4023d4a94f5aef2ac2
Max Jacobson <max@hardscrabble.net>
2023-10-15 00:12:19 -0400

    Run swift-format
    https://github.com/apple/swift-format

--- Assistant/ContentView.swift
+++ Assistant/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Button("Hire an assistant") {
-                let url = URL(string: "https://www.google.com/search?q=hire+personal+assistant+in+my+area")!
-                UIApplication.shared.open(url)
-            }.padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(8)
+  var body: some View {
+    VStack {
+      Button("Hire an assistant") {
+        let url = URL(string: "https://www.google.com/search?q=hire+personal+assistant+in+my+area")!
+        UIApplication.shared.open(url)
+      }
+      .padding()
+              .background(Color.blue)
+      .foregroundColor(.white)
+      .cornerRadius(8)
 
-
-            Text("(This will fix all of your problems)")
-        }
-        .padding()
+      Text("(This will fix all of your problems)")
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
