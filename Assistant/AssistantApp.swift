d92958486d18e9bcab8fe59d223ed45dc56ce6bc
Max Jacobson <max@hardscrabble.net>
2023-10-14 22:33:49 -0400

    Initial Commit

---
+++ Assistant/AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantApp.swift
+//  Assistant
+//
+//  Created by Max Jacobson on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
